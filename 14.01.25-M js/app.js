91099f2c9a6680165593477620e49c9cafe66cbd
skzzbom4545 <meloyansveta399@gmail.com>
2025-01-14 20:27:34 +0400

    Add files via upload

---
+++ 14.01.25-M js/app.js
@@ -0,0 +1,20 @@
+var xndzorkg = 300;
+var kg = prompt("Greq te qani kg xndzor eq cankanum?");
+var vchar = xndzorkg * kg;
+alert("duq petq e vjareq"+ vchar+ " dram");
+
+var x = 0.03;
+var qash = prompt("Greq dzer qashy");
+var jur = x * qash;
+alert("Dzez anhrajesht e " +jur+ " litr jur");
+
+var dollar = 394;
+var euro = 400;
+var rubli = 3.7;
+var dram = prompt("Greq gumari chapy dramov");
+var dollars = dram / dollar;
+var euros = dram / euro;
+var rublis = dram / rubli;
+alert("-Petq e vcharel: "+ dollars + " USD" +                 
+    " -Petq e vcharel: " + euros + " euro" +
+    " -Petq e vcharel: " + rublis + " rubli");
