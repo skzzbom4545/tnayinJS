d1739505483c0b79f8765ec9eb12f51378933d98
skzzbom4545 <meloyansveta399@gmail.com>
2025-01-22 02:31:50 +0400

    Add files via upload

---
+++ 20.01.25-JS/app.js
@@ -0,0 +1,17 @@
+var a = prompt()
+for(var b = 0; b <= a; b++){
+    console.log(b);
+}
+
+var j = prompt()
+for(var s = j; s >= 1; s--){
+    console.log(s);
+}
+
+for(var s = 1; s <= 10; s++){
+    if(s % 2 == 1){
+        document.write("<h1>Hello world</h1>")
+    }else{
+        document.write("<h3>Hello world</h3>")
+    }
+}
